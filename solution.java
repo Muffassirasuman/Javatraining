0870d3269114a3f4dc827528d95c44cf6399caf4
Muffassira Suman M <sumanyasrahafsa@gmail.com>
2025-02-03 15:46:43 +0530

    my first commit

---
+++ solution.java
@@ -0,0 +1,21 @@
+public class solution {
+    public static void main(String[] args) {
+        // System.out.println(9&12);
+        // int a=0b1011;
+        // int a=0xcf;
+        // System.out.println(a);
+        // int a=~6;
+        // System.out.println(a);
+        // int a=~9;
+        // System.out.println(a);
+        boolean a = 6;
+        bool b = 5;
+        if (a^b){
+          System.out.println("Number is odd");
+        }
+        else{
+            System.out.println("Number is even");  
+        }
+
+    }
+}
