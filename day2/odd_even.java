d34430913f64323162ee9deec2cda4d0ed9cfb2c
Muffassira Suman M <sumanyasrahafsa@gmail.com>
2025-02-04 11:25:26 +0530

    placements training

---
+++ day2/odd_even.java
@@ -0,0 +1,11 @@
+public class odd_even {
+    public static void main(String[] args) {
+        int a=20;
+        if(a%2==0){
+            System.out.println("it is a even");
+        }
+        else{
+            System.out.println("it is odd");
+        }
+    }
+}
