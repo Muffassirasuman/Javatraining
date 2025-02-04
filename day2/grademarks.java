0b2d2e6fa67c60aedc67d72384f3bebc62b44988
Muffassira Suman M <sumanyasrahafsa@gmail.com>
2025-02-04 12:10:20 +0530

    placements

---
+++ day2/grademarks.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class grademarks {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      int marks=sc.nextInt();
+      switch (marks) {
+        case 1:
+          System.out.println("your grade is A");
+            break;
+        case 2:
+        System.out.println("your grade is B");
+        case 3:  
+        System.out.println("your grade is c");
+        case 4:
+        System.out.println("your grade is d");
+        default:
+        System.out.println("fail");
+            break;
+      }
+    }
+}
