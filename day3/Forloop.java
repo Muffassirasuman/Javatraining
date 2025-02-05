4ee80456b5fe038723e8b5a342cc40b97f655991
Muffassira Suman M <sumanyasrahafsa@gmail.com>
2025-02-05 10:22:51 +0530

    training

---
+++ day3/Forloop.java
@@ -0,0 +1,22 @@
+public class ForLoop {
+  
+        public static void main(String[] args) {
+            // for(int 1=0;i<=5;i++){
+            //     System.out.println(i);
+            
+            // int i=0;
+            // for(;i<=10;i++){
+            //     System.out.print(i+" ");
+            int i=0;
+
+            for(;i<5;){
+                System.out.print(i+" ");
+                i++;
+            }
+            System.out.println();
+            }
+      
+        }
+    
+    
+
